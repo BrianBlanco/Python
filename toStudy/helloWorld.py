63f873e4af5c21cc376b10a8e086b33aaf0c6413
Brian Giannoni Lillo <briangiannoni@gmail.com>
2020-02-23 15:05:11 +0100

    Add Readme.MD and improve the comments

--- toStudy/helloWorld.py
+++ toStudy/helloWorld.py
@@ -1,1 +1,9 @@
+# This shows "Hello World" in the terminal.
 print ("Hello World")
+
+# This is a comment, it's a lane that the pc doesn't read.
+# You can use this to write down what your code do.
+
+"""
+ This is how you comment more than one lane.
+"""
--- toStudy/variables.py
+++ toStudy/variables.py
@@ -1,8 +1,22 @@
+# Integer, the value has to be a real numbers that do not have fractional values.
 myInt = 7
+
+# Float, the value it's a fractional value
 myFloat = 7.2
+
+# String, it stores text
 myString = "Hello World"
+
+# Boolean, it can be True o false
 myBoolean = True
+
+# Array, it contains a list with all kind of data types
+myArray = ["Brian", "Jose", 2, True]
+
+# Multiple assignment
 uno, dos = 1, 2
+
+# Value assignment
 tres = uno + dos
 
 print ("myInt =", myInt)
@@ -11,6 +25,7 @@ print ("myString =", myString)
 print ("myBoolean =", myBoolean)
 print ("uno =", uno, ", dos =", dos)
 print (uno, "+", dos, "=", tres)
-
+print(myArray
+      )
 # You can show the variables like this too
 print("The value of myInt is = %d" %(myInt))
