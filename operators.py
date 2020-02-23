57ca2060e2255f574f128a25b62c49637b1a8ffd
Brian Giannoni Lillo <briangiannoni@gmail.com>
2020-02-23 03:37:29 +0100

    operators

---
+++ operators.py
@@ -0,0 +1,27 @@
+# Sum
+result = 1 + 2
+print(result)
+
+# Substract
+result = 1 - 2
+print(result)
+
+# Multiplication
+result = 2 * 2
+print(result)
+
+# Division
+result = 2 / 2
+print(result)
+
+# Remainder
+result = 10 % 3
+print(result)
+
+# Square
+result = 2 ** 2
+print(result)
+
+# Multiply Strings
+result = "hello" * 5
+print(result)
